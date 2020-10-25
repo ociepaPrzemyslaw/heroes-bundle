7d38e5668e6d91aeebd7cc23d5448202c7c90e38
MrSiskin <dd7425533>
2020-10-25 12:04:34 +0100

    initial commit

---
+++ heroes-bundle/src/test/java/ExampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ExampleTest {
+
+    @Test
+    void exemple(){
+        fail();
+    }
+
+}
