759f2ba6232e1043b474d6d7f2d5c77c769a452f
MrSiskin <pociepa@gmail.com>
2020-11-07 20:52:32 +0100

    refactoring in test

--- heroes-bundle/src/test/java/pl/ociepa/BoardTest.java
+++ heroes-bundle/src/test/java/pl/ociepa/BoardTest.java
@@ -1,21 +1,36 @@
 package pl.ociepa;
 
+import javafx.scene.layout.CornerRadii;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
+    private Board board;
+    private Creature creature;
+    @BeforeEach
+    void init(){
+        board = new Board();
+        creature = new Creature();
+    }
     @Test
     void shouldAddCreature(){
 
-        Board board = new Board();
-        Creature creature = new Creature();
 
         board.add(new Point(0,0 ), creature);
 
         Creature creatureFromBoard = board.get(0,0);
 
         assertEquals(creature, creatureFromBoard);
+    }
+
+    @Test
+    void shouldReturnNullWhenFliedIsEmpty() {
+
+        Creature creatureFromBoard = board.get(0,0);
+
+        assertNull(creatureFromBoard);
     }
 }
