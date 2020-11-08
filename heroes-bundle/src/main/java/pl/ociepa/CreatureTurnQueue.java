34aa4f8b235f6b5b01215a3e41e8f019de42131f
MrSiskin <pociepa@gmail.com>
2020-11-08 11:43:42 +0100

    queue initial test

---
+++ heroes-bundle/src/main/java/pl/ociepa/CreatureTurnQueue.java
@@ -0,0 +1,11 @@
+package pl.ociepa;
+
+import java.util.Collection;
+
+public class CreatureTurnQueue {
+    private final Collection<Creature> creatures;
+
+    public CreatureTurnQueue(Collection<Creature> aCreatureList) {
+        creatures = aCreatureList;
+    }
+}
