e11ae53d4587b0899b5d496b5910d7e710673bb9
Przemek <pociepa@gmail.com>
2020-11-22 11:22:13 +0100

    refreschGui

--- battle-gui/src/main/java/pl/ociepa/gui/MapTile.java
+++ battle-gui/src/main/java/pl/ociepa/gui/MapTile.java
@@ -1,12 +1,26 @@
 package pl.ociepa.gui;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-class MapTile extends Rectangle{
+class MapTile extends StackPane {
+
+    Rectangle rectangle;
 
     public MapTile() {
-        super(40, 40, Color.WHITE);
-        setStroke(Color.BLACK);
+
+        rectangle = new Rectangle(40, 40, Color.WHITE);
+        rectangle.setStroke(Color.BLACK);
+        getChildren().add(rectangle);
+    }
+
+    void addCreature(String aName){
+        getChildren().add(new Label(aName));
+    }
+
+    void setBackground(Color aColor){
+        rectangle.setFill(aColor);
     }
 }
